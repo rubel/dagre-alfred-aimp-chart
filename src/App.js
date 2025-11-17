2084515adaaf575c3175d527ac261e0cbf7f646d
rubel <rubel.xpress@gmail.com>
2025-11-17 23:20:05 +0600

    dagre d3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { assetList } from "./chartData";
+import D3TopologyDiagram from "./D3TopologyDiagram";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <div className="app">
+    <D3TopologyDiagram allAssets={assetList} backButtonHandler={()=>{}} />
+  </div>;
+};
 
 export default App;
